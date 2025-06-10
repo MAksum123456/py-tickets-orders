7980e8e7770fad931343ea18dccb6e142ada73fc
Max <smolinskijmaksim1@gmail.com>
2025-06-10 11:38:17 +0100

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "cinema",
     "user",
+    "django_filters",
+
 ]
 
 MIDDLEWARE = [
@@ -124,7 +126,7 @@ TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
